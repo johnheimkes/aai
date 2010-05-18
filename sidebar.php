1f39c98811629ced0a23bdec8d28d88d8c0a5236
Jake Dahn <admin@jakedahn.com>
2010-05-18 09:18:35 -0500

    chanigng sidebar to match that of the mockup

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,11 @@
 <div id="sidebar">
   <div class="side_block">
-    <h3>Pages</h3>
-    <ul>
-      <li>stuff</li>
-      <li>stuff</li>
-      <li>stuff</li>
-      <li>stuff</li>
-      <li>stuff</li>
-    </ul>
+    <h3>Did You Know</h3>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus vitae risus vitae lorem iaculis placerat. Aliquam sit amet felis. Etiam congue. Donec risus risus, pretium ac.</p>
+</p>
+    
   </div>
-  <div class="side_block">
+  <!-- <div class="side_block">
     <h3>Heading</h3>
     <ul>
       <li>stuff</li>
@@ -18,5 +14,5 @@
       <li>stuff</li>
       <li>stuff</li>
     </ul>
-  </div>
+  </div> -->
 </div>
